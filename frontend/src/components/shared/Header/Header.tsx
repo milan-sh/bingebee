278b1b6bc010911f9df6c1fdd8366c3263913368
Milan Singh <milan083821@gmail.com>
2025-05-14 18:02:40 +0530

    update home page button

--- frontend/src/components/shared/Header/Header.tsx
+++ frontend/src/components/shared/Header/Header.tsx
@@ -1,12 +1,14 @@
 import { Button } from "@/components/ui/button"
 import Logo from "./Logo"
-import { MoveRight } from "lucide-react"
 
 const Header = () => {
   return (
     <nav className="px-3 pt-6 pb-4 md:px-8 md:pb-2 flex items-center justify-between border-b border-gray-700">
         <Logo/>
-        <Button size={"lg"} variant={"outline"} className="bg-transparent cursor-pointer text-lg py-5 hover:bg-primary hover:text-white flex itece">Get started <span><MoveRight size="50"/></span></Button>
+        <div className="flex items-center justify-between gap-x-4">
+          <Button className="md:text-lg py-4 px-6" variant={"outline"}>Signup</Button>
+          <Button className="md:text-lg py-4 px-6 bg-accent cursor-pointer" >Login</Button>
+        </div>
     </nav>
   )
 }
