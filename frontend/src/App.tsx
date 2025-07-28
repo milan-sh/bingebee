48c869c896cf5f5def687231d76ecd574d777359
Milan Singh <milan083821@gmail.com>
2025-07-28 14:13:59 +0530

    frontend: add shadcn lib

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "./components/ui/button"
+import { Award } from "lucide-react"
 
 const App = () => {
   return (
-    <div>BingeBee</div>
+    <div className="bg-black text-white min-h-screen">
+      <Button variant="default">Heelo</Button>
+      <Award size={44} scale={2}/>
+    </div>
   )
 }
 
