bc5caa6375db634715a8ea72b12aa8c2617004b6
Milan Singh <milan083821@gmail.com>
2025-08-04 23:40:24 +0530

    feat: enhance Footer component with copyright notice and social media links

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,25 @@
+import {Copyright, Github, Linkedin, Twitter} from "lucide-react"
+import { Link } from "react-router"
 
 const Footer = () => {
   return (
-    <div className="bg-black text-white flex items-center justify-center text-2xl">Footer</div>
+    <div className="bg-black text-white flex items-center justify-between text-lg font-semibold p-4">
+      <div className="flex items-center gap-x-2">
+        <Copyright />
+        <span>{new Date().getFullYear()} BingeBee. All rights reserved.</span>
+      </div>
+      <div className="flex justify-between items-center gap-x-4">
+        <Link to="https://github.com/milan-sh" target="_blank" className="hover:text-gray-400">
+          <Github size={25}/>
+        </Link>
+        <Link to="https:/x.com/dev_81milan" target="_blank" className="hover:text-gray-400">
+          <Twitter size={25}/>
+        </Link>
+        <Link to="https://www.linkedin.com/in/milan-singh-81ms33/" target="_blank" className="hover:text-gray-400">
+          <Linkedin size={25} />
+        </Link>
+      </div>
+    </div>
   )
 }
 
