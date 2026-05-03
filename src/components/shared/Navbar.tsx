abef2d43b6fe2dd0eb1a413d4a56a604ea21725e
milan-sh <devmilansingh81@gmail.com>
2026-05-03 18:54:25 +0530

    feat: theme switcher

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -1,13 +1,19 @@
-import { Button } from "../ui/button"
-
+import { Button } from "../ui/button";
+import { Searchbar } from "./Searchbar";
+import { ThemeToggle } from "./Themetoggle";
 
 const Navbar = () => {
   return (
-    <nav>
-        <h1>BingeBee</h1>
-        <Button>Login</Button>
+    <nav className="w-full flex items-center justify-between p-2">
+      <h1 className="font-medium text-xl">BingeBee</h1>
+      <Searchbar />
+      <div className="flex items-center gap-2">
+        <Button variant="outline">Sign Up</Button>
+        <Button className="mr-2">Login</Button>
+        <ThemeToggle />
+      </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,21 +1,22 @@
 import Navbar from "@/components/shared/Navbar";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ThemeProvider } from "@/hooks/theme-provider";
 
 export const Route = createRootRoute({
   component: () => (
     <div>
-      <header>
-        <Navbar/>
-      </header>
-      <main>
-        <Outlet />
-      </main>
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <header>
+          <Navbar />
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </ThemeProvider>
       <TanStackRouterDevtools />
     </div>
   ),
 
-  notFoundComponent: () => (
-    <div>Not found Brother!</div>
-  ),
+  notFoundComponent: () => <div>Not found Brother!</div>,
 });
